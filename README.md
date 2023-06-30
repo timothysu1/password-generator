72e613fc28391938f3128435679e3a1da9ddce46
Timothy <timsu100@gmail.com>
2023-06-29 19:03:37 -0700

    add character types array

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# password-generator
+# Portfolio - Timothy Su
+
+
+## Built With
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [Git](https://git-scm.com/)
+## Deployed Link
+
+* [See Live Site](#)
+
+## Usage
+
+
+## Learning Points 
+
+
+## Author Info
+
+### Timothy Su
+
+* [LinkedIn](https://www.linkedin.com/in/timothysu1/)
+* [Github](https://github.com/timothysu1)
+
+
+## Credits
+Converting all letters in an array to upper case: https://askjavascript.com/how-to-convert-an-array-to-uppercase-in-javascript/
+
+## License
+
+Please refer to license in the repo. 
--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 // Assignment Code
+//querySelector returns first element in doc that matches specified selector
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //assigns variable to the random password created by generatePassword
   var password = generatePassword();
+  //takes the textarea in the html and changes the text to this variable
   var passwordText = document.querySelector("#password");
-
+  //changes variable to the of the password to the randomly generated password
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button so when the button is pressed, a password is generated
+//addEventListener method that
 generateBtn.addEventListener("click", writePassword);
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = lowerCase.map(item => item.toUpperCase());
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "}", "|", "\{", "~"]
+// All of the posibble characters that can be used to make a password
+
+function generatePassword() {
+
+}
+
+//Pseudocode
+//When the button is pressed 
+//Prompt asking how long the password needs to be
+  //User can only choose value between 8 and 128 characters
+//Prompt asking if the password should include lowercase, uppercase, numeric, and/or special characters
+  //User must choose at least one
+//All of the charcter types will be accounted for and a random password will be created
+
+
+
+//Testing
 
